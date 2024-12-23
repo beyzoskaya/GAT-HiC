b4d0f0a42e5bce2e8765f2a76fae44ff1c7f5e1c
Beyza Kaya <105801503+beyzoskaya@users.noreply.github.com>
2024-12-23 17:17:42 +0300

    Update normalize.R

--- normalize.R
+++ normalize.R
@@ -4,10 +4,8 @@ args = commandArgs(trailingOnly=TRUE)
 filename = args[1]
 print(filename)
 filepath = paste("Data_NCol_Hindlll_same_train_test_mESC/Data_GNN_mESC_Hindlll/", filename, ".txt", sep = "")
-#filepath = paste("Data_same_resolution/Data_GNN/", filename, ".txt", sep = "")
 
 adj = as.matrix(read.table(filepath, sep="\t"))
 normed = KRnorm(adj)
 new_name = paste("Data_NCol_Hindlll_same_train_test_mESC/Data_GNN_mESC_Hindlll/", filename, "_KR_normed.txt", sep = "")
-#new_name = paste("Data_same_resolution/Data_GNN/", filename, "_KR_normed.txt", sep = "")
 write.table(normed, file= new_name, row.names=FALSE, col.names=FALSE)
